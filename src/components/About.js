4b1264fac76105322d878cae3c03088ac7267259
darknowns <doublezero13@gmail.com>
2021-04-15 22:17:55 +0700

    okay

--- src/components/About.js
+++ src/components/About.js
@@ -8,7 +8,7 @@ const Home = () => {
       <section>
         <div className="text-white flex flex-col justify-center items-center text-center min-h-screen">
           <img className="w-64 h-64 rounded-full mx-auto" src={avatar} alt="" />{" "}
-          <h1 className="mt-6 text-4xl font-bold">This is my Portofolio</h1>
+          <h1 className="mt-6 text-4xl font-bold">This is my About</h1>
           <h1 className="mt-6 text-sm font-bold underline"><a href="https://darknowns.s-ul.eu/XkAjYbif">Download my Resume</a></h1>
         </div>
       </section>
